1551aa6b652e58a0880d3ec0a9e4bc3c04e11245
AlanoxSwe <61416243+AlanoxSwe@users.noreply.github.com>
2020-10-20 15:59:57 +0200

    workflow

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './styles/main.scss';
 const heading = document.createElement('h1');
 heading.textContent = 'Allan!';
 
+console.log(heading);
+
 // Append heading node to the DOM
 const app = document.querySelector('#root');
 app.append(heading);
