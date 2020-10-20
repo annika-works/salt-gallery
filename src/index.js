6d93446456710e804e0755e7c0b46b91c49a6c6f
annika hallerberg <annika@Salt-admins-MacBook-Pro.local>
2020-10-20 13:33:09 +0200

    next, prev btn

--- src/index.js
+++ src/index.js
@@ -1,20 +1,59 @@
+// const app = document.querySelector('#root');
 /* eslint-disable no-console */
 import axios from 'axios';
 import './styles/main.scss';
+import githubLogo from './assets/GitHub_Logo.png';
 
-// const app = document.querySelector('#root');
+const githubhandler = document.querySelector('footer img');
+const mainElement = document.querySelector('main');
+const searchBtn = document.querySelector('#search-btn');
+const searchInput = document.querySelector('#search-input');
+const nextBtn = document.querySelector('#next-btn');
+const prevBtn = document.querySelector('#prev-btn');
+
+githubhandler.setAttribute('src', githubLogo);
 
 const baseUrl = 'https://api.unsplash.com/search/photos';
-// const envVariables = process.env;
+const API_KEY = 'yX30oA4euofvCkZShYflX41XXG__CP6oiTaZ7AFa19w';
+let search = '';
+let page = 1;
+const maxItems = 10;
 
-// const { API_KEY } = envVariables;
-
-axios.get(`${baseUrl}?per_page=10&page=1&query=cats&client_id=yX30oA4euofvCkZShYflX41XXG__CP6oiTaZ7AFa19w`)
-  .then(response => {
-    // handle success
-    console.log(response);
+const getImages = endpoint => axios.get(endpoint)
+  .then(({ data }) => {
+    const { results } = data;
+    results.forEach(({ urls: images }) => {
+      mainElement.innerHTML += `<img src=${images.small}>`;
+    });
   })
   .catch(error => {
-    // handle error
     console.log(error);
   });
+
+if (page <= 1) prevBtn.setAttribute('disabled', 'disabled');
+
+searchBtn.addEventListener('click', () => {
+  page = 1;
+  search = searchInput.value;
+  const endpoint = `${baseUrl}?per_page=${maxItems}&page=${page}&query=${search}&client_id=${API_KEY}`;
+  mainElement.innerHTML = '';
+  if (search) getImages(endpoint);
+});
+
+nextBtn.addEventListener('click', () => {
+  prevBtn.removeAttribute('disabled');
+  page += 1;
+  const endpoint = `${baseUrl}?per_page=${maxItems}&page=${page}&query=${search}&client_id=${API_KEY}`;
+  mainElement.innerHTML = '';
+  if (search) getImages(endpoint);
+  console.log('next', page);
+});
+
+prevBtn.addEventListener('click', () => {
+  page -= 1;
+  if (page < 2) prevBtn.setAttribute('disabled', 'disabled');
+  const endpoint = `${baseUrl}?per_page=${maxItems}&page=${page}&query=${search}&client_id=${API_KEY}`;
+  mainElement.innerHTML = '';
+  if (search) getImages(endpoint);
+  console.log('prev', page);
+});
